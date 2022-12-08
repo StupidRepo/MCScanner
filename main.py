ac39bceb2f4e2fad0f26369d2e9288dec5170d64
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2022-12-08 16:10:11 +0000

    Update main.py
    Warnings and a better(?) status system.

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ from mcstatus import JavaServer
 import random
 import concurrent.futures
 import pymongo
+import time
 
-ok = False
+servers_found = 0
 
 threads = 4
+
+print("WARNING! Using or modifying the code in any way to turn it into\n"
+      "a DDos program is illegal! This program was made for educational purposes, "
+      "and should not be used in malicious ways! This includes:\nLeaking/selling server IPs, DDosing people or"
+      "Doxxing people is illegal. Do not use this program with malicious intent!")
+print("I made this program for EDUCATIONAL PURPOSES only. If you try to use this program to DDos someone,"
+      "then you are responsible for what happens. Not me!")
+print("Also please read the Minecraft terms and the EULA here:"
+      "\nhttps://www.minecraft.net/en-us/terms\nhttps://www.minecraft.net/en-us/eula")
+print("Remember, using this program maliciously is illegal! DO NOT DDos people!")
+print()
 
 while True:
     try:
         threads = int(input("How many threads should I use (recommended: 4)? "))
         if threads > 32:
-            print("Going above 32 threads will:\nuse so much of your router's bandwidth that it'll slow the whole "
-                  "internet down (and slow the program down)\nslow down your computer "
-                  "significantly or crash it.")
+            print("Going above 32 threads will:\n- use so much of your router's bandwidth that it'll slow your "
+                  "router down (and slow the program down)\n- slow down your computer "
+                  "significantly\nOR\n- crash it.")
         break
     except:
         print("Not a valid number!")
@@ -38,16 +50,20 @@ for a in A:
         ip_range = f"{a}.{b}.0.0/16"
         ip_ranges.append(ip_range)
 
+def doPrintLoop():
+    while True:
+        print(f'\rScanning for servers. Currently found {servers_found} '
+              f'{"server" if servers_found == 1 else "servers" }.',
+              end=' ', flush=True)
+        time.sleep(.5)
+
 def thread(ipr):
-    status = None
     mas = masscan.PortScanner()
     mas.scan(ipr, ports='25565', arguments='--max-rate 105000')
     scan_result = json.loads(mas.scan_result)
     for ip in scan_result['scan']:
-        print("Possible server...")
         server = JavaServer(ip, 25565)
         status = server.status()
-        print("Found a server! Saving to DB!")
         j = {
             'ip': server.address.host,
             'description': status.description,
@@ -55,12 +71,12 @@ def thread(ipr):
             'online_players': status.players.online,
             'max_players': status.players.max
         }
+        global servers_found
+        servers_found += 1
         if mycol.count_documents({'ip': server.address.host}, limit=1) != 0:
-            print("Server already exists in DB! Updating!")
             mycol.update_one({'ip': server.address.host}, j)
         else:
             mycol.insert_one(j)
-            print("Saved to DB!")
 
 if __name__ == '__main__':
     print(f"=====STARTING SCAN=====")
@@ -70,3 +86,6 @@ if __name__ == '__main__':
     executor = concurrent.futures.ThreadPoolExecutor(threads)
     for ip_range in ip_ranges:
         idk = executor.submit(thread, ip_range)
+
+    executor2 = concurrent.futures.ThreadPoolExecutor(1)
+    executor2.submit(doPrintLoop)
