01c6270694627e98a4480bcfc836e30a2c6ec01d
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2022-12-08 20:26:16 +0000

    more stats

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import random
 import concurrent.futures
 import pymongo
 import time
+import sys
 
 servers_found = 0
 
 threads = 4
+seed = random.randrange(sys.maxsize)
 
 text = False
 
@@ -18,9 +20,10 @@ print("WARNING! Using or modifying the code in any way to turn it into\n"
       "Doxxing people is illegal. Do not use this program with malicious intent!")
 print("I made this program for EDUCATIONAL PURPOSES only. If you try to use this program to DDos someone,"
       "then you are responsible for what happens. Not me!")
-print("Also please read the Minecraft terms and the EULA here:"
+print("Make sure to please read the Minecraft terms and the EULA here:"
       "\nhttps://www.minecraft.net/en-us/terms\nhttps://www.minecraft.net/en-us/eula")
 print("Remember, using this program maliciously is illegal! DO NOT DDos people!")
+print("(Oh and also, if you are playing on a multiplayer whilst running this, you will disconnect!")
 print()
 
 while True:
@@ -32,7 +35,18 @@ while True:
                   "significantly\nOR\n- crash it.")
         break
     except:
-        print("Using 4 as the default then.")
+        break
+
+while True:
+    try:
+        seedLocal = input("What seed should the random generator use (default: system time)? ")
+        if seedLocal.strip().__len__() != 0:
+            seed = seedLocal
+        break
+    except:
+        break
+
+rng = random.Random(seed)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
@@ -42,8 +56,8 @@ mycol = mydb["servers"]
 A = list(range(1, 0xff))
 B = list(range(1, 0xff))
 
-random.shuffle(A)
-random.shuffle(B)
+rng.shuffle(A)
+rng.shuffle(B)
 
 ip_ranges = []
 
@@ -54,8 +68,14 @@ for a in A:
 
 def doPrintLoop():
     while True:
-        print(f'\rScanning for servers. Currently found {servers_found} '
-              f'{"server" if servers_found == 1 else "servers" }.',
+        count = mycol.estimated_document_count()
+        start = count - servers_found
+        print(f'\rScanning for servers. Found: {servers_found} '
+              f'{"server" if servers_found == 1 else "servers" }. '
+              f'Servers in DB at start: {start} '
+              f'{"server" if start == 1 else "servers" }, '
+              f'servers in DB now: {count} {"server" if count == 1 else "servers" }. '
+              f'(+{round((count - start) / count * 100, 2)}%)',
               end=' ', flush=True)
         time.sleep(.5)
 
@@ -71,7 +91,7 @@ def thread(ipr):
             'description': status.description,
             'version': status.version.name,
             'online_players': status.players.online,
-            'max_players': status.players.max
+            'max_players': status.players.max,
         }
         global servers_found
         servers_found += 1
@@ -82,8 +102,9 @@ def thread(ipr):
 
 if __name__ == '__main__':
     print(f"=====STARTING SCAN=====")
+    print(f'SEED: {seed}')
 
-    random.shuffle(ip_ranges)
+    rng.shuffle(ip_ranges)
 
     executor = concurrent.futures.ThreadPoolExecutor(threads)
     for ip_range in ip_ranges:
