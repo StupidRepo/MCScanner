7235e654e80a696b173ba449b75909de7f9de40e
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2022-12-09 07:51:11 +0000

    warning

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ text = False
 
 print("WARNING! Using or modifying the code in any way to turn it into "
       "a DDos program is illegal!\nThis program was made for educational purposes, "
-      "and should not be used in malicious ways! This includes:\nLeaking/selling server IPs, DDosing people or "
-      "Doxxing people is illegal. Do not use this program with malicious intent!")
+      "and should not be used in malicious ways! Do not use this program with malicious intent!")
 print("I made this program for EDUCATIONAL PURPOSES only. If you try to use this program to DDos someone, "
       "then you are responsible for what happens. Not me!")
-print("Make sure to please read the Minecraft terms and the EULA here:"
+print("Please read the Minecraft terms and the EULA here:"
       "\nhttps://www.minecraft.net/en-us/terms\nhttps://www.minecraft.net/en-us/eula")
 print("Remember, using this program maliciously is illegal! DO NOT DDos people!")
 print("(Oh and also, if you are playing on a multiplayer server whilst running this, "
