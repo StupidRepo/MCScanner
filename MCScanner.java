691001cf951e4944a68ab380a5515027880f5b6e
Bradlee Barnes <b.barnes@doyouliveinthe.uk>
2023-07-10 15:26:10 +0100

    Accidentally deleted the .java file

---
+++ MCScanner.java
@@ -0,0 +1,140 @@
+import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class MCScanner {
+   public static void main(String[] var0) {
+    int threads = 1024;
+    int timeout = 1000;
+    int minimumRange = 1;
+    int maxRange = 255;
+    int port = 25565;
+
+    String filename = "output.txt";
+
+    float version = 1.07f;
+
+    ArrayList<Thread> threadList = new ArrayList<Thread>();
+
+    System.out.println("MCScanner v" + version + "!");
+    System.out.println("Starting scan...");
+
+    JFrame frame = new JFrame("MCScanner v" + version);
+    frame.setDefaultCloseOperation(3);
+    frame.setSize(300, 100);
+    frame.setLayout(new BorderLayout());
+
+    JProgressBar progressBar = new JProgressBar(0, (maxRange - minimumRange + 1) * 256 * 256);
+    progressBar.setStringPainted(true);
+
+    frame.add(progressBar, "Center");
+
+    JLabel scannedLabel = new JLabel("Scanned: 0/" + (maxRange - minimumRange + 1) * 256 * 256);
+    scannedLabel.setHorizontalAlignment(0);
+    frame.add(scannedLabel, "South");
+    frame.setVisible(true);
+    int scanned = 0;
+
+    for(int i = minimumRange; i <= maxRange; ++i) {
+        for(int j = 0; j <= 255; ++j) {
+            for(int k = 0; k <= 255; ++k) {
+                String ip = "" + i + "." + j + "." + k + ".0";
+
+                Thread scanThread = new Thread(new ScannerThread(ip, port, timeout, filename));
+                threadList.add(scanThread);
+                scanThread.start();
+
+                if (threadList.size() >= threads) {
+                    Iterator iterator = threadList.iterator();
+
+                    while(iterator.hasNext()) {
+                        Thread nextThread = (Thread)iterator.next();
+
+                        try {
+                            nextThread.join();
+                            ++scanned;
+                            progressBar.setValue(scanned);
+                            scannedLabel.setText("Scanned: " + scanned + "/" + (maxRange - minimumRange + 1) * 256 * 256);
+                        } catch (InterruptedException timeout2) {
+                            // eh
+                        }
+                }
+                threadList.clear();
+                }
+            }
+        }
+    }
+
+    Iterator yetAnotherIterator = threadList.iterator();
+
+    while(yetAnotherIterator.hasNext()) {
+        Thread nextThreadAgain = (Thread)yetAnotherIterator.next();
+
+        try {
+            nextThreadAgain.join();
+            ++scanned;
+            progressBar.setValue(scanned);
+            scannedLabel.setText("Scanned: " + scanned + "/" + (maxRange - minimumRange + 1) * 256 * 256);
+        } catch (InterruptedException timeout1) {
+            // well
+        }
+    }
+
+    frame.setVisible(false);
+    frame.dispatchEvent(new WindowEvent(frame, 201));
+    System.out.println("Scan completed!");
+    }
+}
+
+class ScannerThread implements Runnable {
+   private String ip;
+   private int port;
+   private int timeout;
+   private String filename;
+
+   public ScannerThread(String ip, int port, int timeout, String filename) {
+      this.ip = ip;
+      this.port = port;
+      this.timeout = timeout;
+      this.filename = filename;
+   }
+
+   public void run() {
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(this.ip, this.port), this.timeout);
+
+            InputStream inp = socket.getInputStream();
+            OutputStream outp = socket.getOutputStream();
+            
+            outp.write(new byte[]{-2, 1});
+
+            socket.setSoTimeout(this.timeout);
+
+            byte[] bytesVar = new byte[256];
+            int bytesResp = inp.read(bytesVar);
+
+            if (bytesResp > 0) {
+                System.out.println("Found a server!");
+                String var6 = this.ip + "\n";
+                FileWriter var7 = new FileWriter(this.filename, true);
+                var7.write(var6);
+                var7.close();
+            }
+
+            socket.close();
+        } catch (IOException var8) {
+            // well
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,44 +1,20 @@
 # MCScanner
-Uses masscan and MongoDB to scan for servers and save them to DB.
-
-EDIT/NOTE: I will update this soon so that it saves IPs to a text file instead to skip the hassle with a DB. The whole point of the DB was so that I could make a NodeJS program that lists the servers and stuff in charts on a localhost website.
+Now re-made in Java, and not Python!
 
 ## Requirements
-* `pymongo` - `pip3 install pymongo`
-* `mcstatus` - `pip3 install mcstatus`
-* `masscan` - Mac/Linux only! (Windows not supported)
-* Python 3
-* a MongoDB database 
+* [`Java 17`][adopt17]
 
-## Install `masscan`
-* Install masscan for macOS using `brew`, or `sudo apt get` for Ubuntu
-* Install masscan wrapper for python using `pip3 install python-masscan`
-
-## Install a MongoDB database
-### macOS
-* Run `xcode-select --install` in Terminal
-(If you do not have `brew` installed, these instructions won't work until you install `brew`. Please scroll down and read 'Install `brew`' then come back up and run these commands)
-* Run `brew tap mongodb/brew`
-* Run `brew update`
-* Run `brew install mongodb-community@6.0`
-* Run `brew services start mongodb-community@6.0`
-
-MongoDB not opening? Check this:
-<img width="799" alt="ss" src="https://user-images.githubusercontent.com/69256931/206896713-ce3f5270-c4a0-4f62-b1fa-e2015ff6cdd8.png">
-### Linux
-Linux has multiple distributions, so find the instructions to install MongoDB for your distribution [here](https://www.mongodb.com/docs/manual/administration/install-on-linux/).
-
-## Install `brew`
-### macOS only! (Although you can run `brew` on Linux)
-* Run `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"` in Terminal
-* Follow the setup steps
-* Make sure it's in your PATH
-
-## Start Scanning
-`sudo python3 main.py`
-**Make sure you run python3 with sudo or the scanner will *not* work!**
+## Setup
+### macOS + Linux
+1. `cd` into the path to MCScanner (e.g. `cd ~/Desktop/MCScanner`)
+2. Run `javac MCScanner.java`
+3. Now, every time you want to scan for servers, run `java MCScanner` in the MCScanner directory.
+### Windows
+1. Submit a pull request with instructions for how to do it on Windows!
 
 # Warning
 ### **This program was made for educational purposes only, and is not meant to be used in malicious ways**. 
 Do not DDOS the IPs or do *anything* malicious with the IPs. The only thing this program is supposed to be used for is finding random Minecraft servers and joining them!
 Oh, and **don't use this program for griefing**. Please.
+
+[adopt17]: https://adoptium.net/en-GB/download/
